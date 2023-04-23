3590aef1fc6b19b0a501d53ea7e966e4eaf6ffec
Dávid Kopáčik <david.kopacik@gmail.com>
2023-04-23 22:42:05 +0200

    added benefits section to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,44 @@
-import Layout from "../components/Layout/Layout";
+import Layout from "@/components/Layout/Layout";
+import ImageTile from "@/components/ImageTile/ImageTile";
+import ImageTileContainer from "@/components/ImageTileContainer/ImageTileContainer";
+import Divider from "@/components/Divider/Divider";
+
+import { benefits } from "../data/homepage";
 
 export default function Home() {
   return (
     <Layout>
       <h1>Unlimited movies, TV shows, and more</h1>
+      <ImageTileContainer>
+        {benefits.map((data, i) => {
+          const { title, description, imageSrc, imagePosition, imageAlt } =
+            data;
+
+          return i + 1 === benefits.length ? (
+            <ImageTile
+              key={i}
+              title={title}
+              description={description}
+              imageSrc={imageSrc}
+              imagePosition={imagePosition}
+              imageAlt={imageAlt}
+            />
+          ) : (
+            <>
+              <ImageTile
+                key={i}
+                title={title}
+                description={description}
+                imageSrc={imageSrc}
+                imagePosition={imagePosition}
+                imageAlt={imageAlt}
+              />
+
+              <Divider />
+            </>
+          );
+        })}
+      </ImageTileContainer>
     </Layout>
   );
 }
