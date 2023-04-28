ce8392a0bad9dda895e1d519b98a0c264a0603fb
Dávid Kopáčik <david.kopacik@gmail.com>
2023-04-28 23:16:22 +0200

    Input component used in EmailSubscription, removed input styles

--- src/components/EmailSubscription/EmailSubscription.tsx
+++ src/components/EmailSubscription/EmailSubscription.tsx
@@ -1,6 +1,9 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import cx from "classnames";
 
 import Button from "../Button/Button";
+import Input from "../Input/Input";
 
 type EmailSubscription = {
   className?: string;
@@ -12,6 +15,11 @@ const EmailSubscription = ({
   variant,
   ...other
 }: EmailSubscription) => {
+  const [error, setError] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>("");
+
+  const router = useRouter();
+
   const classes = cx(
     "email-subscription w-full border-transparent",
     {
@@ -20,25 +28,57 @@ const EmailSubscription = ({
     className,
   );
 
+  const checkEmailRegex = (inputValue: string) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+
+    if (!emailRegex.test(inputValue)) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+
+    if (inputValue === "") setError(false);
+  };
+
+  const handleChange = (e: any) => {
+    setInputValue(e.target.value);
+
+    checkEmailRegex(inputValue);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!error && !inputValue) {
+      setError(true);
+    }
+
+    if (!error && inputValue) {
+      router.push("/signup/password");
+    }
+  };
+
   return (
     <div className={classes} {...other}>
-      <form className="flex flex-col">
+      <form className="flex flex-col" onSubmit={handleSubmit}>
         <h3 className="email-subscription__title text-lg lg:text-xl">
           Ready to watch? Enter your email to create or restart your membership.
         </h3>
         <div className="pt-4 flex flex-col items-start sm:flex-row">
-          <div className="border-1 relative inline-flex w-full flex-auto max-w-none sm:w-auto sm:max-w-sm">
-            <label className="email-subscription__label absolute leading-6">
-              Email adress
-            </label>
-            <div className="email-subscription__inputWrapper w-full p-0">
-              <input
-                type="email"
-                className="email-subscription__input border rounded border-solid pt-6 px-4 pb-2 w-full leading-6"
-              />
-            </div>
-          </div>
-          <Button variant="start" href="#" className="mt-4 sm:mt-0 sm:ml-2">
+          <Input
+            label="Email adress"
+            type="email"
+            errorMessage="Please enter a valid email address"
+            onChange={(e) => handleChange(e)}
+            error={error}
+            autoComplete="off"
+          />
+
+          <Button
+            variant="start"
+            type="submit"
+            className="mt-4 sm:mt-0 sm:ml-2"
+          >
             Get started
           </Button>
         </div>
