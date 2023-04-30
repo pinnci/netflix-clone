68e7ef3ea5519b57b15c5b3fe9f535963cd7c527
Dávid Kopáčik <david.kopacik@gmail.com>
2023-04-30 13:50:53 +0200

    Added email regex check, handling for focus, blur, change to EmailSubscription

--- src/components/EmailSubscription/EmailSubscription.tsx
+++ src/components/EmailSubscription/EmailSubscription.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import cx from "classnames";
 
 import Button from "../Button/Button";
@@ -17,18 +17,21 @@ const EmailSubscription = ({
 }: EmailSubscription) => {
   const [error, setError] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>("");
+  const [inputField, setInputField] = useState<HTMLInputElement | null>(null);
 
   const router = useRouter();
+
+  const inputRef = useRef(null);
 
   const classes = cx(
     "email-subscription w-full border-transparent",
     {
-      "rounded-lg p-4 border-4": variant === "bordered",
+      "rounded-lg p-4 pb-6 border-4": variant === "bordered",
     },
     className,
   );
 
-  const checkEmailRegex = (inputValue: string) => {
+  const checkEmailRegex = () => {
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
     if (!emailRegex.test(inputValue)) {
@@ -43,20 +46,46 @@ const EmailSubscription = ({
   const handleChange = (e: any) => {
     setInputValue(e.target.value);
 
-    checkEmailRegex(inputValue);
+    if (error) {
+      checkEmailRegex();
+    }
+
+    if (e.target.value === "") {
+      setError(false);
+    }
+  };
+
+  const handleFocus = (e: any) => {
+    if (error && inputValue) {
+      checkEmailRegex();
+    }
+  };
+
+  const handleBlur = (e: any) => {
+    if (inputValue) {
+      checkEmailRegex();
+    }
+  };
+
+  const focusInput = () => {
+    inputField && inputField.focus();
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (!error && !inputValue) {
-      setError(true);
+    if (!inputValue && !error) {
+      focusInput();
     }
 
     if (!error && inputValue) {
       router.push("/signup/password");
     }
   };
+
+  useEffect(() => {
+    setInputField(inputRef.current);
+  }, []);
 
   return (
     <div className={classes} {...other}>
@@ -70,6 +99,9 @@ const EmailSubscription = ({
             type="email"
             errorMessage="Please enter a valid email address"
             onChange={(e) => handleChange(e)}
+            onBlur={(e) => handleBlur(e)}
+            onFocus={(e) => handleFocus(e)}
+            ref={inputRef}
             error={error}
             autoComplete="off"
           />
