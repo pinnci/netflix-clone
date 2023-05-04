e6a84122d1de199e80ae563cd9072aad444d6875
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-04 19:48:23 +0200

    used size prop for Button in HeaderDesktop

--- src/components/Header/HeaderDesktop.tsx
+++ src/components/Header/HeaderDesktop.tsx
@@ -1,21 +1,54 @@
+import cx from "classnames";
+import Link from "next/link";
+
 import Logo from "../Logo/Logo";
 import LanguageSelector from "../LanguageSelector/LanguageSelector";
 import Button from "../Button/Button";
 import Container from "../Container/Container";
 
-const HeaderDesktop = () => {
+type HeaderDesktop = {
+  variant: "notLoggedIn" | "loggedIn" | "registration";
+  className?: string;
+} & React.ComponentProps<"header">;
+
+const HeaderDesktop = ({ variant, className, ...other }: HeaderDesktop) => {
+  const classes = cx(
+    "header__desktop",
+    {
+      "header__desktop--notLoggedIn flex absolute top-0 left-0 w-full sm:px-8 md:px-8 lg:px-12":
+        variant === "notLoggedIn",
+      "header__desktop--registration py-3 flex lg:px-4 lg:py-4":
+        variant === "registration",
+    },
+    className,
+  );
+
   return (
-    <div className="header__desktop flex absolute top-0 left-0 w-full sm:px-8 md:px-8 lg:px-12">
+    <header className={classes} {...other}>
       <Container className="flex justify-between items-center">
         <Logo />
-        <div className="flex">
-          <LanguageSelector className="mx-3 sm:mx-4" />
-          <Button variant="primary" href="/">
-            Sign in
-          </Button>
-        </div>
+
+        {
+          variant === "notLoggedIn" ? (
+            <div className="flex">
+              <LanguageSelector className="mx-3 sm:mx-4" />
+              <Button variant="primary" href="/" size="small">
+                Sign in
+              </Button>
+            </div>
+          ) : variant === "registration" ? (
+            <div className="flex">
+              <Link
+                href="/"
+                className="text-sm font-medium hover:underline md:text-base lg:text-xl"
+              >
+                Sign In
+              </Link>
+            </div>
+          ) : null /**logged in header here */
+        }
       </Container>
-    </div>
+    </header>
   );
 };
 
