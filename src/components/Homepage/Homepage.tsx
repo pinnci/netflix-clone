e8f10f7d8d3e8806655cbccaa5c0d88af4327a69
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-12 18:08:12 +0200

    created Homepage component

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import Layout from "../Layout/Layout";
+import HeroBanner from "../HeroBanner/HeroBanner";
+import ImageTileContainer from "../ImageTileContainer/ImageTileContainer";
+import ImageTile from "../ImageTile/ImageTile";
+import Faq from "../Faq/Faq";
+import Divider from "../Divider/Divider";
+
+import { benefits } from "../../data/homepage";
+
+const Homepage = () => {
+  return (
+    <Layout variant="notLoggedIn">
+      <HeroBanner />
+      <ImageTileContainer>
+        {benefits.map((data, i) => {
+          const { title, description, imageSrc, imagePosition, imageAlt } =
+            data;
+
+          return i + 1 === benefits.length ? (
+            <ImageTile
+              key={title}
+              title={title}
+              description={description}
+              imageSrc={imageSrc}
+              imagePosition={imagePosition}
+              imageAlt={imageAlt}
+            />
+          ) : (
+            <React.Fragment key={title}>
+              <ImageTile
+                key={title}
+                title={title}
+                description={description}
+                imageSrc={imageSrc}
+                imagePosition={imagePosition}
+                imageAlt={imageAlt}
+              />
+
+              <Divider />
+            </React.Fragment>
+          );
+        })}
+      </ImageTileContainer>
+      <Faq />
+    </Layout>
+  );
+};
+
+export default Homepage;
