5dca0ed27a3e97e7ba3979ae360e29be636a1aa6
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-12 15:07:09 +0200

    created data file for LanguageSelector

---
+++ src/data/languageSelector.ts
@@ -0,0 +1,18 @@
+type LanguageSelector = {
+  title: string;
+  lang: string;
+  value: string;
+};
+
+export const languageSelector: LanguageSelector[] = [
+  {
+    title: "English",
+    lang: "en",
+    value: "en-SK",
+  },
+  {
+    title: "Čeština",
+    lang: "cs",
+    value: "cs-SK",
+  },
+];
