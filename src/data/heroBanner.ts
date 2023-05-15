c938c8631f4b20f8e9a336c45137046f1da2d664
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-15 10:58:41 +0200

    added imageSrc and imageAlt to HeroBanner data file

--- src/data/heroBanner.ts
+++ src/data/heroBanner.ts
@@ -1,9 +1,13 @@
 type HeroBanner = {
   title: string;
   subTitle: string;
+  imageSrc: string;
+  imageAlt: string;
 };
 
 export const heroBanner: HeroBanner = {
   title: "Unlimited movies, TV shows, and more.",
   subTitle: "Watch anywhere. Cancel anytime.",
+  imageSrc: "/images/hero-banner.jpg",
+  imageAlt: "Cover picture of Netflix catalogue",
 };
