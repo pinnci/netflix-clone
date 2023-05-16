ff75e4c50002605f02241fb0cfb931699502d62a
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-16 10:39:28 +0200

    used useTranslation hook in Faq

--- src/components/Faq/Faq.tsx
+++ src/components/Faq/Faq.tsx
@@ -1,10 +1,10 @@
 import cx from "classnames";
+import { useTranslation } from "next-i18next";
 
 import Container from "../Container/Container";
 import Accordion from "../Accordion/Accordion";
 import AccordionItem from "../Accordion/AccordionItem";
 
-import { faq } from "../../data/homepage";
 import RegistrationEmailForm from "../RegistrationEmailForm/RegistrationEmailForm";
 
 type Faq = {
@@ -12,6 +12,8 @@ type Faq = {
 } & React.ComponentProps<"div">;
 
 const Faq = ({ className, ...other }: Faq) => {
+  const { t } = useTranslation("homepage");
+
   const classes = cx(
     "faq mt-12 px-4 pt-14 pb-12 md:pt-16 md:px-20 md:pb-16 md:mt-12 lg:pt-14 lg:px-28 lg:pb-14 lg:mt-16 xl:pt-24 xl:px-24 xl:pb-24 xl:mt-14 2xl:pt-24 2xl:px-40 2xl:pb-36 2xl:mt-16",
     className,
@@ -22,16 +24,19 @@ const Faq = ({ className, ...other }: Faq) => {
       <div className={classes} {...other}>
         <div className="flex flex-col">
           <h2 className="font-bold text-3xl text-white sm:text-4xl">
-            Frequently Asked Questions
+            {t("faqTitle")}
           </h2>
           <Accordion>
-            {faq.map((question, i) => {
-              return (
-                <AccordionItem title={question.title} key={i} index={i}>
-                  {question.description}
-                </AccordionItem>
-              );
-            })}
+            {/**@ts-ignore */}
+            {t("faqs", { returnObjects: true }).map(
+              ({ title, description }: any, i: number) => {
+                return (
+                  <AccordionItem title={title} key={i} index={i}>
+                    {description}
+                  </AccordionItem>
+                );
+              },
+            )}
           </Accordion>
 
           <RegistrationEmailForm variant="plain" className="mt-4" />
