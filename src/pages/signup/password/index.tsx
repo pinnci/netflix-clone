ff062fc9a7358596e2b965101110465e287d305b
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-16 17:45:18 +0200

    used useTranslation hook in registration page, added getStaticProps method

--- src/pages/signup/password/index.tsx
+++ src/pages/signup/password/index.tsx
@@ -1,19 +1,29 @@
 import { NextSeo } from "next-seo";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
 
 import Layout from "@/components/Layout/Layout";
 import RegistrationPasswordForm from "@/components/RegistrationPasswordForm/RegistrationPasswordForm";
 
-import { registrationPasswordForm } from "../../../data/registration";
+import type { Locale } from "@/data/languageSelector";
 
 const CreatePassword = () => {
-  const { buttonTitle } = registrationPasswordForm;
+  const { t } = useTranslation("registration");
 
   return (
     <Layout variant="registration">
-      <NextSeo title={buttonTitle} />
+      <NextSeo title={`${t("buttonTitle")}`} />
       <RegistrationPasswordForm />
     </Layout>
   );
 };
 
 export default CreatePassword;
+
+export async function getStaticProps({ locale }: Locale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["registration"])),
+    },
+  };
+}
