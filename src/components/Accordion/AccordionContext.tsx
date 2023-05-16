3be651fa4d064971eeba79629a77bfb61401b1d4
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-16 11:23:48 +0200

    added any to AccordionContext type

--- src/components/Accordion/AccordionContext.tsx
+++ src/components/Accordion/AccordionContext.tsx
@@ -2,7 +2,7 @@ import { createContext } from "react";
 
 type AccordionContextType = {
   activeIndex: string | number | null;
-  setActiveIndex: React.Dispatch<React.SetStateAction<number | null>>;
+  setActiveIndex: React.Dispatch<React.SetStateAction<any>>;
 };
 
 const AccordionState = {
