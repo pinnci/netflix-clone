ad5849d60d123deebb12a80bccd51bc04bf9cda4
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-16 17:00:05 +0200

    used useTranslation hook in login page, added getStaticProps method

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { NextSeo } from "next-seo";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import Container from "@/components/Container/Container";
 import Layout from "../components/Layout/Layout";
@@ -8,17 +10,18 @@ import LoginForm from "../components/LoginForm/LoginForm";
 import { loginForm } from "../data/login";
 
 const Login = () => {
-  const { imageSrc, imageAlt, title } = loginForm;
+  const { t } = useTranslation("login");
+  const { imageSrc } = loginForm;
 
   return (
     <Layout variant="login">
-      <NextSeo title={title} />
+      <NextSeo title={`${t("title")}`} />
       <div className="loginForm__gradient absolute w-full min-h-full"></div>
       <div className="loginForm__imageContainer absolute w-full h-full">
         <Image
           src={imageSrc}
           className="loginForm__image object-cover object-left"
-          alt={imageAlt}
+          alt={t("imageAlt")}
           fill
         />
       </div>
@@ -30,3 +33,15 @@ const Login = () => {
 };
 
 export default Login;
+
+type Locale = {
+  locale: "en" | "cs";
+};
+
+export async function getStaticProps({ locale }: Locale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["login"])),
+    },
+  };
+}
