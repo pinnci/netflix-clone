700d82ed7957fd04435d26f7634df8c23057578a
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-16 10:37:00 +0200

    removed texts from heroBanner data file

--- src/data/heroBanner.ts
+++ src/data/heroBanner.ts
@@ -1,13 +1,7 @@
 type HeroBanner = {
-  title: string;
-  subTitle: string;
   imageSrc: string;
-  imageAlt: string;
 };
 
 export const heroBanner: HeroBanner = {
-  title: "Unlimited movies, TV shows, and more.",
-  subTitle: "Watch anywhere. Cancel anytime.",
   imageSrc: "/images/hero-banner.jpg",
-  imageAlt: "Cover picture of Netflix catalogue",
 };
