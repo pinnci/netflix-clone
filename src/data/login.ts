3cb44c7b0836bf7827117aa5402f92d3fcf63178
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-16 17:00:32 +0200

    removed text data from Login data file

--- src/data/login.ts
+++ src/data/login.ts
@@ -1,37 +1,7 @@
 type LoginForm = {
   imageSrc: string;
-  imageAlt: string;
-  title: string;
-  email: { label: string; errorMessage: string };
-  password: { label: string; errorMessage: string };
-  buttonLabel: string;
-  checkboxLabel: string;
-  helpLabel: string;
-  signUp: {
-    title: string;
-    link: string;
-    href: string;
-  };
 };
 
 export const loginForm: LoginForm = {
   imageSrc: "/images/hero-banner.jpg",
-  imageAlt: "Cover picture of Netflix catalogue",
-  title: "Sign In",
-  email: {
-    label: "Email",
-    errorMessage: "Please enter a valid email.",
-  },
-  password: {
-    label: "Password",
-    errorMessage: "Password is required.",
-  },
-  buttonLabel: "Sign In",
-  checkboxLabel: "Remember me",
-  helpLabel: "Need help?",
-  signUp: {
-    title: "New to Netflix?",
-    link: "Sign up now",
-    href: "/",
-  },
 };
