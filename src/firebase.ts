db56092574c68ae95b21c972ba2069ba8fc4cf8e
Dávid Kopáčik <david.kopacik@gmail.com>
2023-05-18 08:46:15 +0200

    firebase keys moved to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDFtIgGVhUsFpGqoOmkRoIzpQzfitp-9TU",
-  authDomain: "netflix-clone-defec.firebaseapp.com",
-  projectId: "netflix-clone-defec",
-  storageBucket: "netflix-clone-defec.appspot.com",
-  messagingSenderId: "23810165344",
-  appId: "1:23810165344:web:438863b225e52f0af5d105",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
