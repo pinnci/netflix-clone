93072264caa23bfade907e1c28f87dbc4635a2b1
Dávid Kopáčik <david.kopacik@gmail.com>
2023-06-09 18:15:24 +0200

    imported DashboardBanner component to Dashboard, added lorem ipsum

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,19 +1,68 @@
 import Layout from "../Layout/Layout";
 import Container from "../Container/Container";
-
-import { auth } from "../../firebase";
-import { signOut } from "firebase/auth";
+import DashboardBanner from "../DashboardBanner/DashboardBanner";
 
 const Dashboard = () => {
   return (
-    <Layout variant="notLoggedIn">
-      <Container>
-        <button
-          className="text-white relative z-20 pt-40"
-          onClick={() => signOut(auth)}
-        >
-          Log out
-        </button>
+    <Layout variant="loggedIn">
+      <DashboardBanner />
+      <Container className="py-6">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel
+        tincidunt neque, eget sagittis neque. Fusce tincidunt, metus eu bibendum
+        pharetra, justo dolor maximus libero, non convallis diam est
+        sollicitudin nulla. In porttitor enim a erat scelerisque faucibus. Etiam
+        ut luctus diam, sit amet cursus ipsum. Etiam ut nunc dui. Aenean tempus
+        metus in dolor luctus accumsan. Maecenas bibendum ac nunc dapibus
+        suscipit. Vestibulum faucibus convallis sem, ac mollis ex dapibus id.
+        Integer a libero semper, auctor turpis pretium, bibendum urna. Proin
+        vehicula ante sit amet risus tristique, quis dictum felis sollicitudin.
+        Vestibulum ullamcorper, sapien a facilisis finibus, elit mi tincidunt
+        augue, sed consectetur orci turpis ac justo. Mauris lobortis, nibh at
+        tempus aliquam, ipsum nibh dignissim dui, in fringilla purus massa quis
+        mi. Fusce egestas vel magna in fringilla. Sed condimentum, urna ut
+        porttitor tincidunt, velit tellus ullamcorper nulla, eget luctus risus
+        magna non erat. Integer mollis, sem dapibus semper sagittis, arcu felis
+        aliquam orci, ullamcorper maximus elit orci ac turpis. Vestibulum cursus
+        leo vel malesuada mattis. Praesent dapibus, erat vitae volutpat
+        ultrices, dui lectus dictum odio, eu malesuada est quam nec velit.
+        Nullam non egestas diam. Nullam scelerisque mi ligula. Morbi non quam
+        erat. Duis a lorem quis ligula lacinia aliquet mattis condimentum ante.
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere
+        cubilia curae; Aliquam maximus nec leo non vulputate. Maecenas
+        pellentesque facilisis laoreet. Nam faucibus, nulla at tempor dapibus,
+        lectus tellus hendrerit quam, non sollicitudin quam orci non diam. Proin
+        sit amet libero sit amet leo maximus iaculis sit amet eget ante. Sed
+        efficitur metus mauris, non lobortis mi aliquet non. Etiam aliquet non
+        velit ac suscipit. Sed non varius lacus. Phasellus id dui hendrerit,
+        mollis purus non, pharetra lectus. Pellentesque sed tellus nec est
+        condimentum aliquam. Maecenas lobortis nisi ante, at facilisis dolor
+        maximus in. Proin quis turpis tincidunt, fermentum massa in, dignissim
+        lorem. Suspendisse ac massa id nunc scelerisque consequat. Phasellus sed
+        orci cursus, fringilla libero et, porta sapien. Suspendisse consequat
+        lorem sed erat eleifend venenatis et id metus. Nam convallis consequat
+        ligula, et laoreet urna consectetur sit amet. Morbi rhoncus sem at
+        imperdiet viverra. Mauris venenatis, sapien sed pretium pulvinar, ipsum
+        enim malesuada lacus, pharetra eleifend dui sapien id nisl. Sed a
+        vestibulum magna, vel imperdiet risus. Nullam iaculis justo vel nisi
+        consectetur, at lobortis nisi venenatis. Aliquam mauris nibh, placerat
+        at libero sed, aliquet ultrices augue. Quisque et placerat nisi, commodo
+        condimentum tortor. Lorem ipsum dolor sit amet, consectetur adipiscing
+        elit. Curabitur tortor erat, sodales nec metus blandit, consectetur
+        lacinia lacus. Proin odio ex, fringilla a lacus nec, faucibus pulvinar
+        eros. Vestibulum lacinia consequat urna, ut tristique ipsum consectetur
+        nec. Ut vitae metus dictum, semper tortor eget, elementum nisi. Donec
+        laoreet lectus id orci cursus tempus. Sed ultricies odio eget velit
+        faucibus iaculis. Nulla sed purus bibendum, maximus lectus at, cursus
+        ex. Aliquam eget rhoncus libero, id aliquet urna. Nulla et neque a metus
+        tempor pulvinar sed sed sem. Fusce dictum consectetur dui, sed viverra
+        mi lobortis et. Nam lorem risus, posuere eu tellus sit amet, pulvinar
+        commodo dolor. In a arcu non risus tincidunt sollicitudin. In id odio
+        vitae libero rutrum posuere quis id mauris. Duis at aliquam magna, quis
+        efficitur eros. Aliquam ac turpis tincidunt, ornare risus ac, tincidunt
+        ante. Donec a aliquam turpis, non placerat augue. Nulla vulputate dolor
+        id mi scelerisque sagittis. Nunc non risus tortor. Etiam suscipit, enim
+        eget placerat finibus, ipsum tortor convallis lectus, eu fringilla dolor
+        orci at lectus.
       </Container>
     </Layout>
   );
