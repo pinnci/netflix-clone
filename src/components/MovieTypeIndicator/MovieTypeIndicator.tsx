1a273335b13bc5766a65800c447e0152f1f93fe9
Dávid Kopáčik <david.kopacik@gmail.com>
2023-06-09 18:18:39 +0200

    created MovieTypeIndicator component

---
+++ src/components/MovieTypeIndicator/MovieTypeIndicator.tsx
@@ -0,0 +1,53 @@
+import cx from "classnames";
+import Image from "next/image";
+import { useTranslation } from "next-i18next";
+
+type MovieTypeIndicator = {
+  mediaType: string;
+  className?: string;
+} & React.ComponentProps<"div">;
+
+const MovieTypeIndicator = ({
+  mediaType,
+  className,
+  ...other
+}: MovieTypeIndicator) => {
+  const classes = cx("flex mt-4 items-center", className);
+
+  const { t } = useTranslation("dashboard");
+
+  return (
+    <div className={classes} {...other}>
+      {Object.keys(t("media-type", { returnObjects: true })).map((key) => {
+        if (mediaType === key) {
+          const { title } =
+            //@ts-ignore
+            t("media-type", {
+              returnObjects: true,
+            })[key];
+
+          return (
+            <p
+              className="text-base font-medium	text-white xl:text-2xl"
+              key={title}
+            >
+              {title}
+            </p>
+          );
+        }
+      })}
+
+      <div className="relative w-2.5 h-4 ml-2">
+        <Image
+          src="/images/netflix-logo.png"
+          className="object-cover object-left"
+          alt="Netlix logo"
+          fill
+          priority
+        />
+      </div>
+    </div>
+  );
+};
+
+export default MovieTypeIndicator;
