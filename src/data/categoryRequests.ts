301accf6c08c1b1965f30ef4e8408026c5a38bf4
Dávid Kopáčik <david.kopacik@gmail.com>
2023-06-28 11:40:08 +0200

    removed hard coded language query from categoryRequests

--- src/data/categoryRequests.ts
+++ src/data/categoryRequests.ts
@@ -3,9 +3,9 @@ interface Requests {
 }
 
 const requests: Requests = {
-  Trending: `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
+  Trending: `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
   NetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_networks=213`,
-  TopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US`,
+  TopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
   ActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=28`,
   ComedyMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=35`,
   HorrorMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=27`,
