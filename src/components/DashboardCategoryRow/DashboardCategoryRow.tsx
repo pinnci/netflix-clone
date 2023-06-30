ed6c6857abe1561b5aa10cc07cc3c993381e362a
Dávid Kopáčik <david.kopacik@gmail.com>
2023-06-30 08:51:46 +0200

    added DashboardCategoryPopUp component with usage of reactjs-popup library in DashboardCategoryRow

--- src/components/DashboardCategoryRow/DashboardCategoryRow.tsx
+++ src/components/DashboardCategoryRow/DashboardCategoryRow.tsx
@@ -2,13 +2,17 @@ import { useEffect, useState } from "react";
 import cx from "classnames";
 import axios from "axios";
 import Image from "next/image";
-
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Keyboard } from "swiper";
+import Popup from "reactjs-popup";
+import { useTranslation } from "next-i18next";
+
+import DashboardMoviePopUp from "../DashboardMoviePopUp/DashboardMoviePopUp";
 
 type DashboardCategoryRow = {
   title: string;
   fetchUrl: string;
+  currentLocale: string;
   className?: string;
 } & React.ComponentProps<"div">;
 
@@ -18,17 +22,22 @@ type Movie = {
   title: string;
   original_title: string;
   name: string;
+  id: number;
 };
 
 const DashboardCategoryRow = ({
   title,
   fetchUrl,
   className,
+  currentLocale,
   ...other
 }: DashboardCategoryRow) => {
   const [movies, setMovies] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   const classes = cx("dashboardCategoryRow", className);
+
+  const { t } = useTranslation("dashboard");
 
   useEffect(() => {
     axios
@@ -46,9 +55,14 @@ const DashboardCategoryRow = ({
       <p className="text-white text-xl mb-2">{title}</p>
 
       <Swiper
-        navigation={true}
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
         className="dashboardCategoryRow__swiper"
         modules={[Navigation, Keyboard]}
+        onInit={(swiper) => setActiveIndex(swiper.activeIndex)}
+        onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)}
         slidesPerView={2}
         slidesPerGroup={1}
         keyboard={{
@@ -83,23 +97,47 @@ const DashboardCategoryRow = ({
         {movies.map((movie: Movie, i) => {
           return (
             <SwiperSlide key={i}>
-              <div className="dashboardCategoryRow__imageContainer relative">
-                <Image
-                  src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-                  className="object-cover object-center w-auto h-auto"
-                  alt={
-                    movie.title ||
-                    movie.original_title ||
-                    movie.name ||
-                    "Featured title"
-                  }
-                  width={230}
-                  height={150}
+              <Popup
+                mouseEnterDelay={400}
+                trigger={
+                  <div className="dashboardCategoryRow__imageContainer relative">
+                    <Image
+                      src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                      className="object-cover object-center w-auto h-auto"
+                      alt={
+                        movie.title ||
+                        movie.original_title ||
+                        movie.name ||
+                        "Featured title"
+                      }
+                      width={230}
+                      height={150}
+                    />
+                  </div>
+                }
+                offsetX={activeIndex === i ? 45 : 0}
+                keepTooltipInside={activeIndex >= i - 6 ? true : false}
+                position="center center"
+                on={["hover", "focus"]}
+              >
+                <DashboardMoviePopUp
+                  title={movie.title || movie.original_title || movie.name}
+                  movieId={movie.id}
+                  imageSrc={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                  currentLocale={currentLocale}
                 />
-              </div>
+              </Popup>
             </SwiperSlide>
           );
         })}
+        <button
+          className="swiper-button-next"
+          aria-label={`${t("swiperNextButton")}`}
+        ></button>
+        <button
+          className="swiper-button-prev"
+          aria-label={`${t("swiperPrevButton")}`}
+        ></button>
       </Swiper>
     </div>
   );
