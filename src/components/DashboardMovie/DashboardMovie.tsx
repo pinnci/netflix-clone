2c21c43021d4dfa1598a3063da1e6e571c30685a
Dávid Kopáčik <david.kopacik@gmail.com>
2023-11-19 21:41:47 +0100

    refactored DashboardMovie component

--- src/components/DashboardMovie/DashboardMovie.tsx
+++ src/components/DashboardMovie/DashboardMovie.tsx
@@ -1,44 +1,44 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DashboardMoviePopUp from "../DashboardMoviePopUp/DashboardMoviePopUp";
 import Image from "next/image";
+import axios from "axios";
 
 type DashboardMovie = {
   title: string;
   id: number;
-  imageSrc: string;
+  posterPath: string;
+  backdropPath: string;
   currentLocale: string;
 };
 
 const DashboardMovie = ({
   title,
   id,
-  imageSrc,
+  posterPath,
+  backdropPath,
   currentLocale,
 }: DashboardMovie) => {
   const [popUpVisible, setPopUpVisible] = useState<boolean>(false);
   const [popUpContent, setPopUpContent] = useState<{
     title: string;
     id: number;
-    imageSrc: string;
+    posterPath: string;
+    backdropPath: string;
     currentLocale: string;
+    releaseDate: string;
+    firstAirDate: string;
+    lastAirDate: string;
+    runtime: number;
+    genres: [string];
+    tagline: string;
   } | null>(null);
   const [popUpPosition, setPopUpPosition] =
     useState<DashboardMoviePopUp["position"]>("center");
 
-  const handlePopUp = (
-    title: string,
-    id: number,
-    imageSrc: string,
-    currentLocale: string,
-    event: any,
-  ) => {
-    setPopUpContent({
-      title: title,
-      id: id,
-      imageSrc: imageSrc,
-      currentLocale: currentLocale,
-    });
+  let showPopUpTimer: ReturnType<typeof setTimeout>;
+  let hidePopUpTimer: ReturnType<typeof setTimeout>;
 
+  const handlePopUp = (event: any) => {
     setPopUpVisible(true);
 
     //Calculate Pop-up position
@@ -56,28 +56,69 @@ const DashboardMovie = ({
     }
   };
 
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&append_to_response=videos&language=${currentLocale}`,
+      )
+      .then((response) => {
+        setPopUpContent({
+          title: title,
+          id: id,
+          posterPath: `https://image.tmdb.org/t/p/original/${posterPath}`,
+          backdropPath: `https://image.tmdb.org/t/p/original/${backdropPath}`,
+          genres: response.data.genres,
+          currentLocale: currentLocale,
+          releaseDate: response.data.release_date,
+          firstAirDate: response.data.first_air_date,
+          lastAirDate: response.data.last_air_date,
+          runtime: response.data.runtime || response.data.episode_run_time,
+          tagline: response.data.tagline,
+        });
+      })
+      .catch((error) => {
+        console.error("Movie not found!", error);
+
+        axios
+          .get(
+            `https://api.themoviedb.org/3/tv/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&append_to_response=videos&language=${currentLocale}`,
+          )
+          .then((response) => {
+            setPopUpContent({
+              title: title,
+              id: id,
+              posterPath: `https://image.tmdb.org/t/p/original/${posterPath}`,
+              backdropPath: `https://image.tmdb.org/t/p/original/${backdropPath}`,
+              genres: response.data.genres,
+              currentLocale: currentLocale,
+              releaseDate: response.data.release_date,
+              firstAirDate: response.data.first_air_date,
+              lastAirDate: response.data.last_air_date,
+              runtime: response.data.runtime || response.data.episode_run_time,
+              tagline: response.data.tagline,
+            });
+          })
+          .catch((error) => {
+            console.error("TV show not found!", error);
+          });
+      });
+  }, [id, currentLocale, posterPath, backdropPath, title]);
+
   return (
     <>
-      <div
-        className="dashboardCategoryRow__imageContainer relative"
-        onMouseOver={(e) =>
-          setTimeout(() => {
-            handlePopUp(
-              title,
-              id,
-              `https://image.tmdb.org/t/p/original/${imageSrc}`,
-              currentLocale,
-              e,
-            );
-          }, 300)
-        }
-      >
+      <div className="relative">
         <Image
-          src={`https://image.tmdb.org/t/p/original/${imageSrc}`}
-          className="object-cover object-center w-auto h-auto"
+          src={`https://image.tmdb.org/t/p/original/${posterPath}`}
+          className="object-cover object-center w-auto h-auto rounded-md"
           alt={title}
-          width={230}
-          height={150}
+          width={180}
+          height={260}
+          onMouseOver={(e) =>
+            (showPopUpTimer = setTimeout(() => {
+              handlePopUp(e);
+            }, 800))
+          }
+          onMouseLeave={() => clearTimeout(showPopUpTimer)}
         />
       </div>
 
@@ -85,28 +126,20 @@ const DashboardMovie = ({
         <DashboardMoviePopUp
           title={popUpContent.title}
           movieId={popUpContent.id}
-          imageSrc={popUpContent.imageSrc}
+          backdropPath={popUpContent.backdropPath}
           currentLocale={popUpContent.currentLocale}
           onMouseLeave={() =>
-            setTimeout(() => {
+            (hidePopUpTimer = setTimeout(() => {
               setPopUpVisible(false);
-            }, 300)
+            }, 300))
           }
+          genres={popUpContent.genres}
+          tagline={popUpContent.tagline}
           position={popUpPosition}
-        />
-      ) : popUpContent ? (
-        <DashboardMoviePopUp
-          title={popUpContent.title}
-          movieId={popUpContent.id}
-          imageSrc={popUpContent.imageSrc}
-          currentLocale={popUpContent.currentLocale}
-          className="hidden"
-          onMouseLeave={() =>
-            setTimeout(() => {
-              setPopUpVisible(false);
-            }, 300)
-          }
-          position={popUpPosition}
+          runtime={popUpContent.runtime}
+          releaseDate={popUpContent.releaseDate}
+          firstAirDate={popUpContent.firstAirDate}
+          lastAirDate={popUpContent.lastAirDate}
         />
       ) : null}
     </>
