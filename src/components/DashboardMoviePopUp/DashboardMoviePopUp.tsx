9c566f683199d542c0c66004d3d5278bdda9087b
Dávid Kopáčik <david.kopacik@gmail.com>
2023-11-19 21:42:08 +0100

    refactored DashboardMoviePopUp component

--- src/components/DashboardMoviePopUp/DashboardMoviePopUp.tsx
+++ src/components/DashboardMoviePopUp/DashboardMoviePopUp.tsx
@@ -1,14 +1,20 @@
 import cx from "classnames";
-import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Button from "../Button/Button";
 
 type DashboardMoviePopUp = {
   title: string;
   movieId: number;
-  imageSrc: string;
+  backdropPath: string;
+  runtime: number;
+  releaseDate: string;
+  tagline: string;
+  firstAirDate: string;
+  lastAirDate: string;
   className?: string;
+  genres: any;
   currentLocale: string;
   position: "left" | "center" | "right";
 } & React.ComponentProps<"div">;
@@ -16,13 +22,18 @@ type DashboardMoviePopUp = {
 const DashboardMoviePopUp = ({
   title,
   movieId,
-  imageSrc,
+  backdropPath,
   currentLocale,
+  runtime,
+  releaseDate,
+  firstAirDate,
+  lastAirDate,
+  tagline,
+  genres,
   className,
   position,
   ...other
 }: DashboardMoviePopUp) => {
-  const [movieData, setMovieData] = useState<any>(null);
   const [isHovered, setIsHovered] = useState<boolean>(false);
 
   const classes = cx(
@@ -34,27 +45,56 @@ const DashboardMoviePopUp = ({
     className,
   );
 
-  useEffect(() => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&append_to_response=videos&language=${currentLocale}`,
-      )
-      .then((response) => {
-        setMovieData(response.data);
-      })
-      .catch((error) => {
-        axios
-          .get(
-            `https://api.themoviedb.org/3/tv/${movieId}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&append_to_response=videos&language=${currentLocale}`,
-          )
-          .then((response) => {
-            setMovieData(response.data);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-      });
-  }, [movieId, currentLocale]);
+  //Display only year of production
+  const handleDate = (
+    release_date: string,
+    first_air_date: string,
+    last_air_date: string,
+  ) => {
+    let result;
+
+    if (release_date) {
+      const splittedReleaseDate = release_date.split("-");
+
+      result = splittedReleaseDate[0];
+    } else if (!release_date && first_air_date && last_air_date) {
+      const splittedFirstAirDate = first_air_date.split("-");
+      const splittedLastAirDate = last_air_date.split("-");
+
+      result = `${splittedFirstAirDate[0]} - ${splittedLastAirDate[0]}`;
+    } else {
+      return null;
+    }
+
+    return (
+      <span className="block text-base font-light text-neutral-400 mr-1">
+        {result}
+      </span>
+    );
+  };
+
+  //Display runtime in correct format.
+  //If it's under 60mins, show only mins, otherwise calculate hours and show both
+  const handleRuntimeFormat = (runtime: number | Array<string>) => {
+    let result;
+
+    if (typeof runtime === "number" && runtime < 60) {
+      result = `| ${runtime}m`;
+    } else if (typeof runtime === "number" && runtime > 60) {
+      const hours = Math.trunc(runtime / 60);
+      const minutes = runtime - hours * 60;
+
+      result = `| ${hours}h ${minutes}m`;
+    } else {
+      return null;
+    }
+
+    return (
+      <span className="block text-base font-light text-neutral-400">
+        {result}
+      </span>
+    );
+  };
 
   return (
     <Link
@@ -65,28 +105,49 @@ const DashboardMoviePopUp = ({
     >
       <div className="dashboard-movie-pop-up overflow-hidden" {...other}>
         <Image
-          src={imageSrc}
-          className="object-cover object-center h-auto w-full rounded-md"
+          src={backdropPath}
+          className="object-cover object-center h-auto w-full rounded-t-md"
           alt={title}
           width={400}
           height={250}
         />
 
-        <div className="dashboard-movie-pop-up__content p-4">
-          <p className="text-white text-lg mb-4">{title}</p>
+        <div className="dashboard-movie-pop-up__content p-5">
+          <h3 className="text-white text-3xl mb-1">{title}</h3>
 
-          <div className="flex flex-wrap">
-            {movieData?.genres.map((genre: { name: string }, i: number) => {
-              return (
-                <div className="flex items-center" key={i}>
-                  <span className="text-white font-light">{genre.name}</span>
+          {tagline ? (
+            <span className="block text-base font-light text-neutral-400 mb-2">
+              {tagline}
+            </span>
+          ) : null}
 
-                  {i + 2 > movieData.genres.length ? null : (
-                    <span className="text-gray-500 mx-1">•</span>
-                  )}
-                </div>
-              );
-            })}
+          <div className="flex flex-wrap items-center mb-4">
+            <div className="flex items-center mt-1 mr-2">
+              {genres.slice(0, 3).map((genre: { name: string }, i: number) => {
+                return (
+                  <span
+                    className="text-neutral-400 border border-neutral-400 rounded-3xl py-1 px-2 uppercase text-xs font-normal block mr-1.5"
+                    key={i}
+                  >
+                    {genre.name}
+                  </span>
+                );
+              })}
+            </div>
+
+            {handleDate(releaseDate, firstAirDate, lastAirDate)}
+
+            {handleRuntimeFormat(runtime)}
+          </div>
+
+          <div className="flex justify-between">
+            <Button variant="primary" size="medium">
+              Play
+            </Button>
+
+            <Button variant="primary" size="medium">
+              More
+            </Button>
           </div>
         </div>
       </div>
