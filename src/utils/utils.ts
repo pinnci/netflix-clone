5c5061cd9d3e1aa934591cddac10b0e5c06f3580
Dávid Kopáčik <david.kopacik@gmail.com>
2023-12-02 08:42:44 +0100

    moved handleDate and handleRuntimeFormat function to utils file

---
+++ src/utils/utils.ts
@@ -0,0 +1,44 @@
+//Display only year of production
+const handleDate = (
+  release_date: string,
+  first_air_date: string,
+  last_air_date: string,
+) => {
+  let result;
+
+  if (release_date) {
+    const splittedReleaseDate = release_date.split("-");
+
+    result = splittedReleaseDate[0];
+  } else if (!release_date && first_air_date && last_air_date) {
+    const splittedFirstAirDate = first_air_date.split("-");
+    const splittedLastAirDate = last_air_date.split("-");
+
+    result = `${splittedFirstAirDate[0]} - ${splittedLastAirDate[0]}`;
+  } else {
+    return null;
+  }
+
+  return result;
+};
+
+//Display runtime in correct format.
+//If it's under 60mins, show only mins, otherwise calculate hours and show both
+const handleRuntimeFormat = (runtime: number | Array<string>) => {
+  let result;
+
+  if (typeof runtime === "number" && runtime < 60) {
+    result = `| ${runtime}m`;
+  } else if (typeof runtime === "number" && runtime > 60) {
+    const hours = Math.trunc(runtime / 60);
+    const minutes = runtime - hours * 60;
+
+    result = `| ${hours}h ${minutes}m`;
+  } else {
+    return null;
+  }
+
+  return result;
+};
+
+export { handleDate, handleRuntimeFormat };
