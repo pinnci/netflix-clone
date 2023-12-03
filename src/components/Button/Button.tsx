149aea31124f42da781194f95881c95a456798eb
Dávid Kopáčik <david.kopacik@gmail.com>
2023-12-03 13:18:23 +0100

    refactored Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,20 +5,26 @@ import Link from "next/link";
 import Icon from "../Icon/Icon";
 
 type Button = {
-  variant: "primary" | "secondary" | "start";
+  variant: "primary" | "secondary" | "tertiary" | "start" | "close";
+  shape: "rounded" | "square";
   size: "small" | "medium" | "large";
   className?: string;
   href?: string;
-  tag?: string | React.ReactNode;
+  icon?: {
+    name: Icon["name"];
+    size: Icon["size"];
+    className?: Icon["className"];
+  };
 } & React.ComponentProps<"button">;
 
 const Button = ({
   variant,
-  className,
-  children,
+  shape,
   size,
+  className,
   href,
-  tag = "button",
+  icon,
+  children,
   type = "button",
   ...other
 }: Button) => {
@@ -26,22 +32,30 @@ const Button = ({
     "button flex flex-row items-center",
     {
       [`button--${variant}`]: variant,
-      "font-medium text-white rounded-md": variant === "start",
-      "font-medium text-white rounded": variant === "primary",
-      "py-1 px-4 text-sm": size === "small",
-      "py-2 px-4 text-base sm:py-3 sm:px-6": size === "medium",
-      "py-2 px-4 text-lg sm:text-2xl sm:py-3 sm:px-6": size === "large",
+      "rounded-full p-4": shape === "rounded",
+      "rounded-md": shape === "square",
+      "py-1 px-4 text-sm": size === "small" && shape !== "rounded",
+      "py-2 px-4 text-base": size === "medium" && shape !== "rounded",
+      "py-2 px-4 text-lg sm:text-2xl sm:py-3 sm:px-6":
+        size === "large" && shape !== "rounded",
     },
     className,
   );
 
   return href ? (
-    <Link href={href} className={classes}>
+    //@ts-ignore
+    <Link href={href} className={classes} {...other}>
+      {icon ? (
+        <Icon name={icon.name} size={icon.size} className={icon.className} />
+      ) : null}
       {children}
       {variant === "start" ? <Icon name="chevron-right" /> : null}
     </Link>
   ) : (
     <button type={type} className={classes} {...other}>
+      {icon ? (
+        <Icon name={icon.name} size={icon.size} className={icon.className} />
+      ) : null}
       {children}
       {variant === "start" ? <Icon name="chevron-right" /> : null}
     </button>
