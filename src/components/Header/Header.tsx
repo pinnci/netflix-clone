747267eeb069c8756b8c40bcd7cdc206146242f9
Dávid Kopáčik <david.kopacik@gmail.com>
2024-01-20 16:07:18 +0100

    created HeaderError component and added support for error variant in Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,20 @@
+import { Layout } from "../Layout/Layout";
+
 import HeaderNotLoggedIn from "./HeaderNotLoggedIn";
 import HeaderRegistration from "./HeaderRegistration";
 import HeaderLogin from "./HeaderLogin";
 import HeaderLoggedIn from "./HeaderLoggedIn";
+import HeaderError from "./HeaderError";
 
-export type Header = {
-  variant: "notLoggedIn" | "loggedIn" | "registration" | "login";
-};
-
-const Header = ({ variant }: Header) => {
+const Header = ({ variant }: Layout) => {
   return variant === "notLoggedIn" ? (
     <HeaderNotLoggedIn />
   ) : variant === "registration" ? (
     <HeaderRegistration />
   ) : variant === "login" ? (
     <HeaderLogin />
+  ) : variant === "error" ? (
+    <HeaderError />
   ) : (
     <HeaderLoggedIn />
   );
