7eb302995e8b9e31129ca15f371b5475fe6c16c8
Dávid Kopáčik <david.kopacik@gmail.com>
2024-02-22 12:47:19 +0100

    changed trending request to show only movies

--- src/data/categoryRequests.ts
+++ src/data/categoryRequests.ts
@@ -3,7 +3,7 @@ interface Requests {
 }
 
 const requests: Requests = {
-  Trending: `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+  Trending: `https://api.themoviedb.org/3/trending/movie/week?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
   NetflixOriginals: `https://api.themoviedb.org/3/discover/tv?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_networks=213`,
   TopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
   ActionMovies: `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&with_genres=28`,
