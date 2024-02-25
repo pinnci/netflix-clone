a7bb8ed4b15c982a7c5dd8eef75af90e6989b1e4
Dávid Kopáčik <david.kopacik@gmail.com>
2024-02-25 12:44:11 +0100

    created page for latest movies

---
+++ src/pages/browse/latest.tsx
@@ -0,0 +1,53 @@
+import { NextSeo } from "next-seo";
+import type { Locale } from "@/data/languageSelector";
+import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+import Layout from "../../components/Layout/Layout";
+import Container from "../../components/Container/Container";
+
+const Latest = () => {
+  const [locale, setLocale] = useState<Locale["locale"]>("en");
+
+  const { t } = useTranslation("common");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setLocale(router.locale === "cs" ? "cs" : "en");
+  }, [router.locale]);
+
+  return (
+    <>
+      <NextSeo
+        title={`${t("defaultTitle")}`}
+        description={`${t("description")}`}
+        openGraph={{ description: `${t("openGraph.description")}` }}
+        additionalMetaTags={[{ name: "keywords", content: `${t("keywords")}` }]}
+      />
+
+      <Layout variant="loggedIn">
+        <Container className="py-6 pt-20 min-h-screen">
+          <h1 className="text-white">Latest</h1>
+        </Container>
+      </Layout>
+    </>
+  );
+};
+
+export default Latest;
+
+export async function getStaticProps({ locale }: Locale) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "dashboard",
+        "modal",
+        "popup",
+      ])),
+    },
+  };
+}
