17486e646e6bf82dd12ea58902bdd6396190344f
Dávid Kopáčik <david.kopacik@gmail.com>
2024-03-04 10:18:04 +0100

    used MovieData types, refactored DashboardMoviePopUp to be fetching data on client side

--- src/components/DashboardMoviePopUp/DashboardMoviePopUp.tsx
+++ src/components/DashboardMoviePopUp/DashboardMoviePopUp.tsx
@@ -2,49 +2,35 @@ import cx from "classnames";
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
 import { useEffect, useState } from "react";
-import Button from "../Button/Button";
 import { createPortal } from "react-dom";
+
+import Button from "../Button/Button";
 
 import {
   handleDate,
   handleRuntimeFormat,
   handleStringToUrl,
+  getPopUpData,
+  MovieData,
 } from "../../utils/utils";
-
-import { MovieData } from "../../utils/utils";
 
 type DashboardMoviePopUp = {
   isOpened: boolean;
-  trigger: null | HTMLCanvasElement;
+  trigger: HTMLCanvasElement | null;
+  id: MovieData["id"];
+  mediaType: MovieData["mediaType"];
+  locale: MovieData["locale"];
+  className?: string;
   onClose: () => void;
   onPopUpClick: () => void;
-  className?: string;
-} & Omit<
-  MovieData,
-  | "posterPath"
-  | "overview"
-  | "productionCompanies"
-  | "productionCountries"
-  | "spokenLanguages"
-  | "videos"
->;
+};
 
 const DashboardMoviePopUp = ({
-  title,
-  originalTitle,
-  originalName,
   trigger,
   isOpened = false,
   id,
-  backdropPath,
   mediaType,
   locale,
-  runtime,
-  releaseDate,
-  firstAirDate,
-  lastAirDate,
-  tagline,
-  genres,
   className,
   onClose,
   onPopUpClick,
@@ -53,6 +39,7 @@ const DashboardMoviePopUp = ({
   const [transitionType, setTransitionType] = useState<
     "fadeIn" | "fadeOut" | null
   >(null);
+  const [movieData, setMovieData] = useState<MovieData | null>(null);
 
   const [popUpStyles, setPopUpStyles] = useState<{
     top: number;
@@ -118,6 +105,18 @@ const DashboardMoviePopUp = ({
     }
   }, [isOpened, trigger, popUpRoot]);
 
+  useEffect(() => {
+    const config = {
+      id,
+      locale,
+      mediaType,
+    };
+
+    getPopUpData(config, (response) => {
+      setMovieData(response);
+    });
+  }, [id, locale, mediaType]);
+
   const handleClose = () => {
     setTimeout(() => {
       setTransitionType("fadeOut");
@@ -126,100 +125,106 @@ const DashboardMoviePopUp = ({
     }, 0);
   };
 
-  if (!isOpened && !trigger) return null;
+  return movieData && isOpened && trigger
+    ? createPortal(
+        <div
+          className={classes}
+          onMouseLeave={handleClose}
+          style={popUpStyles}
+          role="dialog"
+          aria-hidden={!isOpened}
+          tabIndex={-1}
+          aria-label={movieData.title}
+          autoFocus={true}
+          {...other}
+        >
+          <div className="dashboard-movie-pop-up overflow-hidden" {...other}>
+            <Image
+              src={movieData.backdropPath}
+              className="object-cover object-center h-auto w-full rounded-t-md"
+              alt={movieData.title}
+              width={400}
+              height={250}
+              priority
+            />
 
-  return createPortal(
-    <div
-      className={classes}
-      onMouseLeave={handleClose}
-      style={popUpStyles}
-      role="dialog"
-      aria-hidden={!isOpened}
-      tabIndex={-1}
-      aria-label={title}
-      autoFocus={true}
-      {...other}
-    >
-      <div className="dashboard-movie-pop-up overflow-hidden" {...other}>
-        <Image
-          src={backdropPath}
-          className="object-cover object-center h-auto w-full rounded-t-md"
-          alt={title}
-          width={400}
-          height={250}
-          priority
-        />
+            <div className="dashboard-movie-pop-up__gradient absolute top-0 left-0 right-0 bottom-0"></div>
 
-        <div className="dashboard-movie-pop-up__gradient absolute top-0 left-0 right-0 bottom-0"></div>
+            <div className="dashboard-movie-pop-up__content p-5">
+              <h3 className="text-white text-3xl mb-1">{movieData.title}</h3>
 
-        <div className="dashboard-movie-pop-up__content p-5">
-          <h3 className="text-white text-3xl mb-1">{title}</h3>
+              {movieData.tagline && (
+                <span className="block text-base font-extralight text-white mb-2">
+                  {movieData.tagline}
+                </span>
+              )}
 
-          {tagline && (
-            <span className="block text-base font-extralight text-white mb-2">
-              {tagline}
-            </span>
-          )}
+              <div className="flex flex-wrap items-center mt-1 mb-4">
+                <div className="flex items-center mr-2">
+                  {movieData.genres
+                    .slice(0, 3)
+                    .map((genre: { name: string }, i: number) => {
+                      return (
+                        <span
+                          className="text-white border border-white rounded-3xl py-1 px-2 uppercase text-xs font-extralight block mr-1.5"
+                          key={i}
+                        >
+                          {genre.name}
+                        </span>
+                      );
+                    })}
+                </div>
 
-          <div className="flex flex-wrap items-center mt-1 mb-4">
-            <div className="flex items-center mr-2">
-              {genres.slice(0, 3).map((genre: { name: string }, i: number) => {
-                return (
-                  <span
-                    className="text-white border border-white rounded-3xl py-1 px-2 uppercase text-xs font-extralight block mr-1.5"
-                    key={i}
-                  >
-                    {genre.name}
-                  </span>
-                );
-              })}
+                <time className="block text-base font-thin text-white mr-1">
+                  {handleDate(
+                    movieData.releaseDate,
+                    movieData.firstAirDate,
+                    movieData.lastAirDate,
+                  )}
+                </time>
+
+                <time className="block text-base font-thin text-white">
+                  {handleRuntimeFormat(movieData.runtime)}
+                </time>
+              </div>
+
+              <div className="flex justify-between">
+                <Button
+                  variant="secondary"
+                  size="medium"
+                  shape="square"
+                  className="text-black"
+                  href={`/watch/${mediaType}-${id}-${handleStringToUrl(
+                    movieData.originalTitle || movieData.originalName,
+                  )}`}
+                  onClick={(event) => {
+                    event.stopPropagation();
+                  }}
+                  icon={{ name: "play", size: "small", className: "mr-2" }}
+                >
+                  {`${t("play")}`}
+                </Button>
+
+                <Button
+                  variant="tertiary"
+                  size="medium"
+                  shape="square"
+                  className="text-white"
+                  icon={{ name: "info", size: "small", className: "mr-2" }}
+                  onClick={() => {
+                    handleClose();
+                    onPopUpClick();
+                  }}
+                >
+                  {`${t("moreInformation")}`}
+                </Button>
+              </div>
             </div>
-
-            <time className="block text-base font-thin text-white mr-1">
-              {handleDate(releaseDate, firstAirDate, lastAirDate)}
-            </time>
-
-            <time className="block text-base font-thin text-white">
-              {handleRuntimeFormat(runtime)}
-            </time>
           </div>
-
-          <div className="flex justify-between">
-            <Button
-              variant="secondary"
-              size="medium"
-              shape="square"
-              className="text-black"
-              href={`/watch/${mediaType}-${id}-${handleStringToUrl(
-                originalTitle || originalName,
-              )}`}
-              onClick={(event) => {
-                event.stopPropagation();
-              }}
-              icon={{ name: "play", size: "small", className: "mr-2" }}
-            >
-              {`${t("play")}`}
-            </Button>
-
-            <Button
-              variant="tertiary"
-              size="medium"
-              shape="square"
-              className="text-white"
-              icon={{ name: "info", size: "small", className: "mr-2" }}
-              onClick={() => {
-                handleClose();
-                onPopUpClick();
-              }}
-            >
-              {`${t("moreInformation")}`}
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>,
-    popUpRoot,
-  );
+        </div>,
+        popUpRoot,
+      )
+    : null;
 };
 
 export default DashboardMoviePopUp;
